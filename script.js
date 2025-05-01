f783a53e4595bf696715f2472178d70512511c0d
Jzani <jzani894@gmail.com>
2025-05-01 18:16:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) slide.classList.add('active');
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 1000);
